3308f43ca36492d8651d815cffedc9a23037eabe
MassimilianoRozza <rozza.ma.01@gmail.com>
2025-11-04 16:18:35 +0100

    feat(release): Final V1 - Initial stable release of Easy Volley Scout with Capacitor integration

---
+++ android/app/src/main/java/com/easy/volleyscout/MainActivity.java
@@ -0,0 +1,5 @@
+package com.easy.volleyscout;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.easy.volleyscout',
+  appName: 'Easy Volley Scout',
+  webDir: 'build'
+  bundledWebRuntime: false
+};
+
+export default config;
